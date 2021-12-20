7709b870a34b92a787333609f786084ba879faae
Adhinarayanan <uadhinarayanan@gmail.com>
2021-12-20 10:47:09 +0530

    springboot1

---
+++ loanmanagement/src/main/java/citibank/loanmanagement/Customer.java
@@ -0,0 +1,45 @@
+package citibank.loanmanagement;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer_deta")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int cid;
+
+	String customerName;
+
+	int age;
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ loanmanagement/src/main/java/citibank/loanmanagement/service/CustomerService.java
@@ -0,0 +1,9 @@
+package citibank.loanmanagement.service;
+
+import citibank.loanmanagement.pojo.CustomerPojo;
+
+public interface CustomerService {
+
+	CustomerPojo saveCustomer(CustomerPojo customerPojo);
+
+}
