d399131eeb846d4cc07abbe7a2ec7737619f42a3
Adhinarayanan <uadhinarayanan@gmail.com>
2021-12-20 16:23:55 +0530

    2 crud flow13

---
+++ loanmanagement/src/java/citibank/loanmanagement/service/CustomerService.java
@@ -0,0 +1,11 @@
+package citibank.loanmanagement.service;
+
+import citibank.loanmanagement.pojo.CustomerPojo;
+
+public interface CustomerService {
+
+	CustomerPojo saveCustomer(CustomerPojo customerPojo);
+	
+	CustomerPojo getCustomerint (int id);
+
+}
